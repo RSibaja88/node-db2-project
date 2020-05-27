b83fb110d92b9d6f3f65961af028f40cd94c05fb
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-05-26 23:06:35 -0400

    MVP met

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -7,42 +7,41 @@ const router = express.Router();
 //Get Cars
 router.get("/", (req, res) => {
     db('cars')
-    .then(cars => {
-        res.json(cars);
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Failed to GET cars"});
-    });
+      .then(cars => {
+      res.json(cars);
+      })
+      .catch(err => {
+      res.status(500).json({ message: "Failed to GET cars"});
+      });
 });
 
 //Get Cars by ID
 router.get("/:id", (req, res) => {
-    const { id } = req.params;
-
-    db('cars').where({ id }).car()
-    .then(car => {
-        res.json(car);
-    })
-    .catch(err => {
-        res.status(500).json({ message: "Failed to get car by id"});
-    });
+    const { id } = req.params
+    db('cars')
+      .where({ id })
+      .first()
+      .then((car) => {
+      res.json(car)
+      })
+      .catch((err) => {
+      res.status(500).json({ message: "Failed GET car by ID" })
+      });
 });
 
 //POST a Car
 router.post("/", (req, res) => {
-    const carSpecs = req.body;
-    db('cars').insert(carSpecs)
-    .then(ids => {
-        db('cars').where({ id: ids[0] })
-        .then(newCar => {
-            res.status(201).json(newCar)
-        });        
-    })
-    .catch(err => {
-        console.log('POST error', err);
-        res.status(500).json({ message: "Couldnt add car" });
-    });
-});
+    const newCar = req.body;
+      db('cars')
+        .insert(newCar)
+        .then((car) => {
+        res.status(201).json(car);
+        })
+        .catch((error) => {
+        console.log(error);
+        res.status(400).json({ errorMessage: "Failed to POST car" });
+        });
+  });
 
 
 module.exports = router;
--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('cars').del()
+  return knex('cars').truncate() //or del()
     .then(function () {
       // Inserts seed entries
       return knex('cars').insert([
